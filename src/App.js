633d26ba2640144b57a593c19f64b5eeff711a2c
Guru <djguruwap@gmail.com>
2019-12-09 12:09:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchMovie } from './store/actions/movieAction'
 import './App.css';
+import Input from './components/Input';
+import Card from './components/Card';
+import Loader from './components/Loader'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    searchInput: 'The Rain'
+  }
+
+  componentDidMount() {
+    this.props.getMovieList(this.state.searchInput)
+  }
+
+  _getMovie = () => {
+    this.props.getMovieList(this.state.searchInput)
+  }
+
+  _onChangeHandler = (e) => {
+    this.setState({
+      searchInput: e.target.value
+    })
+    console.log(this.state.searchInput)
+  }
+  render() {
+    const { data, loading } = this.props.movies
+    return (
+      <div className="center">
+        <div>
+          <h2 className="center white-text">Movie Search</h2>
+        </div>
+
+        <div className="container">
+          <Input
+            value={this.state.searchInput}
+            onChange={this._onChangeHandler}
+            onClick={this._getMovie}
+          />
+          <div className="row">
+            {
+              loading ? <Loader /> :
+                data.map(item => (
+                  <Card
+                    key={item.imdbID}
+                    image={item.Poster}
+                    name={item.Title}
+                    year={item.Year}
+                  />
+                ))
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getMovieList: (name) => dispatch(fetchMovie(name))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
